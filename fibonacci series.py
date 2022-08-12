e7f678260b1acead50005055846efbdd2458e5f9
DPPrarthana <111065819+DPPrarthana@users.noreply.github.com>
2022-08-12 21:32:03 +0530

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,15 @@
+term = int(input("enter the number of integer"))
+n1=0
+n2=1
+count=0
+if term>=0:
+    print("enter integer")
+else:
+    print("fibonacci series")
+    while count<term:
+     print(n1)
+    if count>term:
+     n=n1+n2
+     n1=n2
+     n2=n
+     coun+1
